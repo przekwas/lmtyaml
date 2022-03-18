e566d246e7d3df058da4b0072b27615d3874d7ce
Luke Przkwas <luke@covalence.io>
2022-03-18 13:29:31 -0500

    initial commit

---
+++ src/App/index.jsx
@@ -0,0 +1,15 @@
+import { BeakerIcon } from '@heroicons/react/solid';
+
+export default function App() {
+	return (
+		<main>
+			<section>
+				<div>
+					<h1>Hello World</h1>
+					<button className="btn btn-primary">Button Test</button>
+					<BeakerIcon className="h-5 w-5 text-blue-500" />
+				</div>
+			</section>
+		</main>
+	);
+}
